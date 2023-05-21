fd5f09c7a0af087846a23c09108657df53f8ae69
NurluhanKakpan <nurluhankakpan@gmail.com>
2023-05-21 18:25:41 +0600

    Creating method put in BST java

--- src/BST.java
+++ src/BST.java
@@ -12,4 +12,24 @@ public class BST<K extends Comparable<K>, V> {
             this.val = val;
         }
     }
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    // Recursive helper method to insert a key-value pair into the binary search tree
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            return new Node(key, val);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.val = val; // Update value if key already exists
+        }
+        return node;
+    }
 }
