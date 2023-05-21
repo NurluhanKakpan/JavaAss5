a24ce30266b391492d2b11205d663303d570f4b5
NurluhanKakpan <nurluhankakpan@gmail.com>
2023-05-21 18:26:43 +0600

    Creating method get in BST java

--- src/BST.java
+++ src/BST.java
@@ -16,7 +16,6 @@ public class BST<K extends Comparable<K>, V> {
         root = put(root, key, value);
     }
 
-    // Recursive helper method to insert a key-value pair into the binary search tree
     private Node put(Node node, K key, V val) {
         if (node == null) {
             return new Node(key, val);
@@ -28,8 +27,27 @@ public class BST<K extends Comparable<K>, V> {
         } else if (cmp > 0) {
             node.right = put(node.right, key, val);
         } else {
-            node.val = val; // Update value if key already exists
+            node.val = val;
         }
         return node;
+    }
+    public V get(K key) {
+        Node node = get(root, key);
+        return node != null ? node.val : null;
+    }
+
+    // Recursive helper method to search for a key in the binary search tree
+    private Node get(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node;
+        }
     }
 }
